87286e93b87da3795676eb6677f4446eb3f806c6
emt2cy <emt2cy@virginia.edu>
2015-06-16 16:25:55 +0200

    updating 99bottles

--- EmilyTrojan-emt2cy/exercises/d2/99bottles.rb
+++ EmilyTrojan-emt2cy/exercises/d2/99bottles.rb
@@ -1,10 +1,9 @@
 number = 99 
-numbernext = number
 
-while numbernext > 2
-	puts numbernext.to_s + "bottles of beer on the wall." + numbernext.to_s + "bottles of beer."
-numbernext = numbernext - 1
-	puts "take one down, pass it around." + numbernext.to_s + "Bottles of beer on the wall."
+while number > 2
+	puts number.to_s + "bottles of beer on the wall." + number.to_s + "bottles of beer."
+number = number - 1
+	puts "take one down, pass it around." + number.to_s + "Bottles of beer on the wall."
 end
 puts "2 bottles of beer on the wall. 2 bottles of beer. Take one down, pass it around. 1 bottle of beer on the wall."
 puts "1 bottle of beer on the wall. 1 bottle of beer on the wall. Take one down, pass it around. No bottles of beer on the wall."
