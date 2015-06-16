02d5d80c5c0b7e420bc59b4c80bd69bb578daf4a
Amanda Halacy <amh7cc@virginia.edu>
2015-06-16 09:47:26 -0400

    6.16 committing

---
+++ AmandaHalacy-ahalacy/day 2/day2b_hw.rb
@@ -0,0 +1,10 @@
+day2b_hw.rb
+
+puts "what is your first name?"
+first name = gets.chomp
+puts "what is your middle name?"
+middle name = gets.chomp
+puts "what is your last name?"
+last name = gets.chomp
+greeting = "hello, "
+puts = "greeting" + "first name " + "middle name " + "last name"
