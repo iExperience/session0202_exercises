3f4d011c4157a667ef0783dcdcf9e4a77ed181f6
Chris <christopher.cahill@aol.com>
2015-06-16 15:56:59 +0200

    Added edits to grandma

--- ChrisCahill-christophercahill/d2/deafgma.rb
+++ ChrisCahill-christophercahill/d2/deafgma.rb
@@ -1,9 +1,12 @@
 puts "*says something to grandma*"
 
-what_you_said = gets.chomp 
 
-if what_you_said == what_you_said.upcase 
-	puts "NO, NOT SINCE 1938!"
-else
-	puts "HUH?! SPEAK UP, SONNY!"
-end 
+while true 
+	what_you_said = gets.chomp 
+	if what_you_said == what_you_said.upcase 
+		puts "NO, NOT SINCE 1938!"
+		break 
+	else
+		puts "HUH?! SPEAK UP, SONNY!"
+	end 
+end
