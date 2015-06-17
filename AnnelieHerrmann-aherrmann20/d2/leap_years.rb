415b16045af96843f2707a345499d67e6e17fc87
aherrmann20 <aherrmann@college.harvard.edu>
2015-06-17 07:15:57 +0200

    Finally figured out leap_years

--- AnnelieHerrmann-aherrmann20/d2/leap_years.rb
+++ AnnelieHerrmann-aherrmann20/d2/leap_years.rb
@@ -1,24 +1,24 @@
 puts "Please give me a starting year."
 start_year = gets.chomp
 start_year_i = start_year.to_i
-add_year = 0
 
 puts "Please give me an ending year."
 end_year = gets.chomp
 end_year_i = end_year.to_i
 
-if (end_year_i % 100 ==0 && end_year_i % 400) || end_year_i % 4 == 0
-	#works if end year is leap year
-	years_between = end_year_i - start_year_i
-	leap_between = years_between/4 - 1
-	puts leap_between
+working_year = start_year_i
 
-else
-	#only works if end year is not a leap year
-	#otherwise the leap years between are actually one less than years_between/4
-	years_between = end_year_i - start_year_i
-	leap_between = years_between/4
-	puts leap_between
+puts
+puts "Look at all these leap years!"
+
+while working_year <= end_year_i
+	if working_year % 4 == 0 && (working_year % 100 != 0 || working_year % 400 == 0)
+			puts working_year.to_s
+	end
+
+	working_year = working_year + 1
 end
+
+puts
 
 
