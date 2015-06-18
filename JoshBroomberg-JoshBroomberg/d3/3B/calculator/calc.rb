c9de74848716a55f2bccb27385d25c829c0e5cc3
JoshBroomberg <joshbroomberg@gmail.com>
2015-06-18 08:57:51 +0200

    update personal site with anchors. Fix flaws in calculater.

--- JoshBroomberg-JoshBroomberg/d3/3B/calculator/calc.rb
+++ JoshBroomberg-JoshBroomberg/d3/3B/calculator/calc.rb
@@ -5,7 +5,7 @@ get "/add/:number_1/:number_2" do |number_1, number_2|
 end
 
 get "/multiply/:number_1/:number_2" do |number_1, number_2|
-	"The solution to #{number_1} time #{number_2} is #{number_1.to_i*number_2.to_i}"
+	"The solution to #{number_1} times #{number_2} is #{number_1.to_i*number_2.to_i}"
 end
 
 get "/minus/:number_1/:number_2" do |number_1, number_2|
@@ -13,7 +13,7 @@ get "/minus/:number_1/:number_2" do |number_1, number_2|
 end
 
 get "/divide/:number_1/:number_2" do |number_1, number_2|
-	"The solution to #{number_1} plus #{number_2} is #{number_1.to_i/number_2.to_i}"
+	"The solution to #{number_1} divided by #{number_2} is #{number_1.to_i/number_2.to_i}"
 
 end
 
