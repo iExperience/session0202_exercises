eb8c0cca9dc68f54d832c0269d9f5b9f92070af4
samuelcg <samuel.gonzalesluna@gmail.com>
2015-06-18 09:29:29 +0200

    Complete exercises2B. Add d3 personal_app.

--- SammyGonzalesLuna-samuelcg/d2/exercises2B/Deaf_Grandma.rb
+++ SammyGonzalesLuna-samuelcg/d2/exercises2B/Deaf_Grandma.rb
@@ -1,22 +1,34 @@
-print "Say something to your grandma: "
-talkToGrandma = gets.chomp
-puts ""
+conversation_over = false
+bye_counter =0
 
-upperCase = talkToGrandma.upcase
-conversationOver = false
-byeCounter = 0
+while !conversation_over
+	print "Say something to your grandma: "
+	words_to_grandma = gets.chomp
+	puts ""
+	upper_case = words_to_grandma.upcase
 
-while !conversationOver
 	print "Grandma: "
-	if talkToGrandma == upperCase
+	if words_to_grandma == upper_case
+		
 		puts "NO, NOT SINCE 1938!"
-	elsif talkToGrandma == "Bye"
-		byeCounter += 1
-		if byeCounter == 3
-			conversationOver = true
+		puts ""
+	elsif words_to_grandma == "BYE"
+		
+		bye_counter += 1
+		if bye_counter == 3
+			
+			conversation_over = true
+		else
+			
+			puts "HUH?!? SPEAK UP, SONNY!"
+			puts ""	
 		end
 	else
+		
 		puts "HUH?!? SPEAK UP, SONNY!"
+		puts ""
 	end
+
 end
+
 
