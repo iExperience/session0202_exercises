efa211e43e04163c067cc47bf59caedbcbb58345
emt2cy <emt2cy@virginia.edu>
2015-06-25 09:25:39 +0200

    added edit,update,destroy to lekker app

--- EmilyTrojan-emt2cy/exercises/d8/lekker_plekke/db/schema.rb
+++ EmilyTrojan-emt2cy/exercises/d8/lekker_plekke/db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20150624194635) do
   create_table "places", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
-    t.string   "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
