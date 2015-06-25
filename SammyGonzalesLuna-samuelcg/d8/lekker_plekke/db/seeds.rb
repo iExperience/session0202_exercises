6c9488c533e432545c9263a38972fbb53157775c
samuelcg <samuel.gonzalesluna@gmail.com>
2015-06-25 08:59:34 +0200

    Make index and show action and appropriate views

--- SammyGonzalesLuna-samuelcg/d8/lekker_plekke/db/seeds.rb
+++ SammyGonzalesLuna-samuelcg/d8/lekker_plekke/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "To dev: Creating places...."
+
+Place.delete_all
+
+Place.create! name:"Once in Cape Town", rating: 5,
+  info: "A really cool hostel on Kloof street, join for the Braai!"
+Place.create! name:"Knead Bakery", rating: 4,
+  info: "A great spot for breakfast, sometimes people have to wait a bit for muesli though."
+Place.create! name:"Sababa", rating: 5,
+  info: "Come here for an awesome home style meal! (hint: Lamb knuckle stew!)"
