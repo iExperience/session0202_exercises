13b3e6d53676f3b2b19b10ac907db81ddc2a9025
Chris <christopher.cahill@aol.com>
2015-06-26 09:36:43 +0200

    Recommitting b/c Once wifi was questionable

--- ChrisCahill-christophercahill/d8/lekker_plekke/db/schema.rb
+++ ChrisCahill-christophercahill/d8/lekker_plekke/db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625161037) do
+ActiveRecord::Schema.define(version: 20150626071857) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
-    t.string   "title"
     t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "place"
     t.integer  "place_id"
   end
 
